5c9cc17827cd45db14bed4f9895de4a51e8fa3ac
NguyenQuangThanh-2948 <q.thanh10022004@gmail.com>
2026-05-04 23:47:58 +0700

    1

---
+++ De_Tai_TT/src/main/java/com/example/de_tai_tt/DeTaiTtApplication.java
@@ -0,0 +1,13 @@
+package com.example.de_tai_tt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeTaiTtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeTaiTtApplication.class, args);
+    }
+
+}
---
+++ De_Tai_TT/src/test/java/com/example/de_tai_tt/DeTaiTtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.de_tai_tt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeTaiTtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
